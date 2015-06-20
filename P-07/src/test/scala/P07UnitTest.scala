482157bfbf717e2bb01e938ac62e71f31506a17c
Arpan Chaudhury <Arpan.Chaudhury@crest.in>
2015-06-20 17:43:15 +0530

    Arpan - modifying test of problem 7

--- P-07/src/test/scala/P07UnitTest.scala
+++ P-07/src/test/scala/P07UnitTest.scala
@@ -1,14 +1,14 @@
 import org.specs2._
 
-class P07UnitTest extends Specification { 
+class P07UnitTest extends Specification { def is = s2"""
+
+    This is a specification to check flatten method
+
+    flatten method
+      returns flattened list                           $e1
+      returns Nil when list is empty                   $e2"""
 
 	val p07 = new P07
-	def is = s"""
-	This is a specification to check flatten method
-
-	  flatten method
-		returns flattened list                           $e1
-		returns Nil when list is empty                   $e2"""
 
 	def e1 = p07.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) === List(1, 1, 2, 3, 5, 8)
 
